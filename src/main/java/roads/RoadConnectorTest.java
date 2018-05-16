5f603aae970862adc2d786bf39f09652077dae28
Nik <nikhil@pricecheckpay.com>
2018-05-16 11:49:49 -0400

    commit with new readme

--- src/main/java/roads/RoadConnectorTest.java
+++ src/main/java/roads/RoadConnectorTest.java
@@ -1,17 +1,9 @@
 package roads;
 
 import static com.jayway.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.Test;
 
-import com.jayway.restassured.response.Response;
-import com.jayway.restassured.response.ValidatableResponse;
+
 
 
 public class RoadConnectorTest extends FunctionalTest {
