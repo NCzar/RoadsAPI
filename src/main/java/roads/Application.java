d9f4300b03bd71fb27be4fe4a38c2c3ee2392a2b
Rajkumar <rajkumar.subramani@dowjones.com>
2019-04-04 09:52:27 -0400

    Added method comment

--- src/main/java/roads/Application.java
+++ src/main/java/roads/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+//Initialize spring boot
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
